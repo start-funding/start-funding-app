551477b20add807cee6b60dba204ba09c8871d25
ACd3v <58480609+alexcalabrese@users.noreply.github.com>
2022-09-08 21:25:46 +0200

    Added id and other fields

--- backend/models/Campaign.js
+++ backend/models/Campaign.js
@@ -1,10 +1,25 @@
+const { v4: uuidv4 } = require('uuid');
 class Campaign {
 
-    constructor(name = "", date_end = -1, min_algo = -1, state = false) {
-        this.name = name;
-        this.date_end = date_end;
+    constructor(
+        title = "", description = "", img = "",
+        min_algo = -1, obj_algo = -1,
+        state = false,
+        date_start = -1, date_end = -1, created = new Date) {
+
+        this.id = uuidv4();
+        this.title = title;
+        this.description = description;
+        this.img = img;
+
         this.min_algo = min_algo;
+        this.obj_algo = obj_algo;
+
         this.state = state;
+
+        this.date_start = date_start;
+        this.date_end = date_end;
+        this.created = created;
     }
 }
 
