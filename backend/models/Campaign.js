54b9cb16f3504ff3c6a95fc3fc8572c880710017
ACd3v <58480609+alexcalabrese@users.noreply.github.com>
2022-09-08 18:42:27 +0200

    Updated campaign constructor

--- backend/models/Campaign.js
+++ backend/models/Campaign.js
@@ -1,8 +1,11 @@
 class Campaign {
 
-    constructor(name, date_end, min_algo, state) {
+    constructor(name = "", date_end = -1, min_algo = -1, state = false) {
         this.name = name;
         this.date_end = date_end;
         this.min_algo = min_algo;
+        this.state = state;
     }
 }
+
+module.exports = Campaign
