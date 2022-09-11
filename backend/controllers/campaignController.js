9c8f5bd5c40b2233b3123a6b50c25033fe8d5a35
ACd3v <58480609+alexcalabrese@users.noreply.github.com>
2022-09-11 18:12:25 +0200

    Added firebase, getAll, get, updated create method

--- backend/controllers/campaignController.js
+++ backend/controllers/campaignController.js
@@ -1,7 +1,62 @@
 const Campaign = require('../models/Campaign.js');
+const { db } = require('../firebase.js');
 
-// create function for post campaign route
-const create = (req, res) => {
+const getAll = async(req, res) => {
+    // Fetch data from db
+    const campaigns = await db.collection('campaigns').get();
+
+    // Check if there are campaigns
+    if (campaigns.empty) {
+        res.status(200).json({
+            message: "No matching documents.",
+            data: null,
+            error: null
+        });
+    }
+
+    // Map data as json
+    let result = campaigns.docs.map(doc => doc.data());
+
+    res.status(200).json({
+        message: "List of all campaign",
+        data: result,
+        error: null
+    });
+}
+
+const get = async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        // Fetch data from db
+        const campaign = await db.collection('campaigns').doc(`${id}`).get();
+
+        const result = campaign.data();
+
+        if (!result) {
+            return res.status(500).json({
+                message: "No matching documents.",
+                data: null,
+                error: null,
+            });
+        }
+
+        res.status(200).json({
+            message: "Campaign details.",
+            data: result,
+            error: null
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "No matching documents.",
+            data: null,
+            error: error,
+        });
+    }
+}
+
+// Create function for post campaign route
+const create = async(req, res) => {
     try {
         const {
             owner,
@@ -24,7 +79,11 @@ const create = (req, res) => {
             endingDate
         );
 
-        // TODO: save new campaign inside db
+        // Saving new campaign inside db
+        const campaignRef = db.collection('campaigns').doc(`${new_campaign.id}`);
+        const result = await campaignRef.set({
+            ...new_campaign
+        });
 
         res.status(201).json({
             message: "Campaign created successfully!",
@@ -39,4 +98,4 @@ const create = (req, res) => {
     }
 };
 
-module.exports = { create };
+module.exports = { getAll, get, create };
