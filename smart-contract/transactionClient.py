ed13b9178136bc1cb5485a941b10202672bdb5c9
ACd3v <58480609+alexcalabrese@users.noreply.github.com>
2022-10-01 17:33:36 +0200

    Added refund call to client

--- smart-contract/transactionClient.py
+++ smart-contract/transactionClient.py
@@ -30,18 +30,26 @@ def demo():
       
     app_id, app_addr, txid = app_client.create()
     print(f"- Created App with id: {app_id} and address addr: {app_addr} in tx: {txid}")
+    
+    result = app_client.call(Crowfunding.set_db_id, db_id="ciao")
+    
+    result = app_client.call(Crowfunding.get_db_id)
+    print(f"- Currrent db_id value: {result.return_value}")
 
-    result = app_client.call(Crowfunding.set_end_date, end_date=1664601223)
+    result = app_client.call(Crowfunding.set_end_date, end_date=1674601223)
     print(f"- Currrent end_date value: {result.return_value}")
 
-    result = app_client.call(Crowfunding.set_target, target=2 * consts.algo)
+    result = app_client.call(Crowfunding.set_target, target=8 * consts.algo)
     print(f"- Currrent target value: {result.return_value}")
     
     get_account_balance(account.address)
+    
+    # app_client.opt_in()
+    # print("- Opted in!")
 
     sp = app_client.client.suggested_params()
     
-    amount = 1
+    amount = 2
     
     txn = TransactionWithSigner(
             txn=transaction.PaymentTxn(account.address, sp, app_addr, amount * consts.algo),
@@ -49,30 +57,60 @@ def demo():
             )
             
     result = app_client.call(Crowfunding.donate, donation=txn)
+    # print(vars(result))
     # print(f"- 1) Collected: {result.return_value}")
     print(f"- 1) Sended: {amount}")
     get_account_balance(account.address)
     
     txn = TransactionWithSigner(
-            txn=transaction.PaymentTxn(account.address, sp, app_addr, amount * consts.algo),
+            txn=transaction.PaymentTxn(account.address, sp, app_addr, 4 * consts.algo),
             signer=account.signer,    
             )
             
     result = app_client.call(Crowfunding.donate, donation=txn)
     # print(f"- 2) Collected: {result.return_value}")
-    print(f"- 2) Sended: {amount}")
+    # print(f"- 2) Sended: {amount}")
+    print("- 2) Sended: 4")
     get_account_balance(account.address)
     
     result = app_client.call(Crowfunding.get_collected)
     print(f"- Smart Contract Balance: {result.return_value}")
     get_account_balance(account.address)
     
-    result = app_client.call(Crowfunding.claim)
-    print("# Claimed!" + "\n")
+    # result = app_client.call(Crowfunding.claim)
+    # print("# Claimed!" + "\n")
     
     result = app_client.call(Crowfunding.get_collected)
     print(f"- Smart Contract Balance: {result.return_value}")
     get_account_balance(account.address)
+    
+    # txn = TransactionWithSigner(
+    #         txn=transaction.PaymentTxn(account.address, sp, app_addr, 8 * consts.algo),
+    #         signer=account.signer,    
+    #         )
+            
+    new_account = accounts.pop()
+    
+    print("----------------")
+    
+    print(f"- new_account: {new_account.address}")
+    get_account_balance(new_account.address)
+    
+    result = app_client.call(Crowfunding.refund, account=new_account.address, amount=2 * consts.algo)
+    print("# Refund requested of amount: 2")
+    
+    get_account_balance(new_account.address)
+    
+    result = app_client.call(Crowfunding.get_collected)
+    print(f"- Smart Contract Balance: {result.return_value}")
+    
+    result = app_client.call(Crowfunding.refund, account=new_account.address, amount=4 * consts.algo)
+    print("# Refund 2 requested of amount: 4")
+    
+    get_account_balance(new_account.address)
+    
+    result = app_client.call(Crowfunding.get_collected)
+    print(f"- Smart Contract Balance: {result.return_value}")
 
 
 if __name__ == "__main__":
