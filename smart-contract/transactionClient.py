57e22f53e02eaaba4168106eac0624e142c8f9b9
ACd3v <58480609+alexcalabrese@users.noreply.github.com>
2022-10-03 22:10:14 +0200

    Refactored client and saved abi json file

--- smart-contract/transactionClient.py
+++ smart-contract/transactionClient.py
@@ -2,6 +2,7 @@ from pyteal import Balance, Global
 from crowfunding import Crowfunding
 from beaker.client import ApplicationClient, LogicException
 from beaker import sandbox
+from algosdk import account
 from algosdk.future import transaction
 from algosdk.atomic_transaction_composer import *
 from beaker import (
@@ -9,22 +10,45 @@ from beaker import (
 )
 from algosdk.v2client import algod
 
+
+# Testnet Purestake
+# algod_address = "https://testnet-algorand.api.purestake.io/ps1"
+# algod_token = "eS30dDuWI3451FsNsPJ6f6KrfpCtK9yHXh1bGhy6"
+# purestake_header = {'X-Api-key': algod_token}
+# algod_client = algod.AlgodClient(algod_token, algod_address, headers=purestake_header)
+
+# Sandbox
 algod_address = "http://localhost:4001"
 algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 algod_client = algod.AlgodClient(algod_token, algod_address)
-
 
 def get_account_balance(my_address):    
     account_info = algod_client.account_info(my_address)
     print("- Account balance: {} Algos".format(account_info.get('amount') / 1e6) + "\n")
 
+def make_donation(account, sp, app_addr, amount, app_client):
+    print(f"($) Sended: {amount}")
+
+    txn = TransactionWithSigner(
+            txn=transaction.PaymentTxn(account.address, sp, app_addr, amount * consts.algo),
+            signer=account.signer,    
+            )
+            
+    return app_client.call(Crowfunding.donate, donation=txn)
+
 def demo():
-    client = sandbox.get_algod_client()
-    accounts = sandbox.get_accounts()
+    # Sandbox
+    algod_address = "http://localhost:4001"
+    algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+    client = sandbox.get_algod_client(algod_address, algod_token)
+    
+    accounts = sandbox.get_accounts(wallet_name="MyWallet")
+    print(accounts)
     account = accounts.pop()
     
+    get_account_balance(account.address)
     print(f"- Account address: {account.address}")
-    # print(f"- Account signature: {account.signer}")
     
     app_client = ApplicationClient(client, Crowfunding(), signer=account.signer)
       
@@ -47,71 +71,58 @@ def demo():
     # app_client.opt_in()
     # print("- Opted in!")
 
-    sp = app_client.client.suggested_params()
+    # sp = app_client.client.suggested_params()
     
-    amount = 2
+    # result = make_donation(account, sp, app_addr, 2, app_client)
+    # get_account_balance(account.address)
+
+    # result = make_donation(account, sp, app_addr, 4, app_client)
+    # get_account_balance(account.address)
     
-    txn = TransactionWithSigner(
-            txn=transaction.PaymentTxn(account.address, sp, app_addr, amount * consts.algo),
-            signer=account.signer,    
-            )
+    # result = app_client.call(Crowfunding.get_collected)
+    # print(f"- Smart Contract Balance: {result.return_value}")
+    # get_account_balance(account.address)
+    
+    # # result = app_client.call(Crowfunding.claim)
+    # # print("# Claimed!" + "\n")
+    
+    # # result = app_client.call(Crowfunding.get_collected)
+    # # print(f"- Smart Contract Balance: {result.return_value}")
+    # # get_account_balance(account.address)
+    
             
-    result = app_client.call(Crowfunding.donate, donation=txn)
-    # print(vars(result))
-    # print(f"- 1) Collected: {result.return_value}")
-    print(f"- 1) Sended: {amount}")
-    get_account_balance(account.address)
+    # new_account = accounts.pop()
     
-    txn = TransactionWithSigner(
-            txn=transaction.PaymentTxn(account.address, sp, app_addr, 4 * consts.algo),
-            signer=account.signer,    
-            )
-            
-    result = app_client.call(Crowfunding.donate, donation=txn)
-    # print(f"- 2) Collected: {result.return_value}")
-    # print(f"- 2) Sended: {amount}")
-    print("- 2) Sended: 4")
-    get_account_balance(account.address)
+    # print("----------------")
     
-    result = app_client.call(Crowfunding.get_collected)
-    print(f"- Smart Contract Balance: {result.return_value}")
-    get_account_balance(account.address)
+    # print(f"- new_account: {new_account.address}")
+    # get_account_balance(new_account.address)
     
-    # result = app_client.call(Crowfunding.claim)
-    # print("# Claimed!" + "\n")
+    # result = app_client.call(Crowfunding.refund, account=new_account.address, amount=2 * consts.algo)
+    # print("# Refund requested of amount: 2")
     
-    result = app_client.call(Crowfunding.get_collected)
-    print(f"- Smart Contract Balance: {result.return_value}")
-    get_account_balance(account.address)
+    # get_account_balance(new_account.address)
     
-    # txn = TransactionWithSigner(
-    #         txn=transaction.PaymentTxn(account.address, sp, app_addr, 8 * consts.algo),
-    #         signer=account.signer,    
-    #         )
-            
-    new_account = accounts.pop()
+    # result = app_client.call(Crowfunding.get_collected)
+    # print(f"- Smart Contract Balance: {result.return_value}")
     
-    print("----------------")
+    # result = app_client.call(Crowfunding.refund, account=new_account.address, amount=4 * consts.algo)
+    # print("# Refund 2 requested of amount: 4")
     
-    print(f"- new_account: {new_account.address}")
-    get_account_balance(new_account.address)
+    # get_account_balance(new_account.address)
     
-    result = app_client.call(Crowfunding.refund, account=new_account.address, amount=2 * consts.algo)
-    print("# Refund requested of amount: 2")
-    
-    get_account_balance(new_account.address)
-    
-    result = app_client.call(Crowfunding.get_collected)
-    print(f"- Smart Contract Balance: {result.return_value}")
-    
-    result = app_client.call(Crowfunding.refund, account=new_account.address, amount=4 * consts.algo)
-    print("# Refund 2 requested of amount: 4")
-    
-    get_account_balance(new_account.address)
-    
-    result = app_client.call(Crowfunding.get_collected)
-    print(f"- Smart Contract Balance: {result.return_value}")
+    # result = app_client.call(Crowfunding.get_collected)
+    # print(f"- Smart Contract Balance: {result.return_value}")
 
 
 if __name__ == "__main__":
-    demo()
+    import json
+
+    crow = Crowfunding()
+    print(crow.approval_program)
+    print(crow.clear_program)
+    
+    with open('abi.json', 'w') as f:
+        json.dump(crow.contract.dictify(), f)
+    # abi = json.dumps(crow.contract.dictify())
+    # demo()
